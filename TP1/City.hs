0d57052a9139b52bbb390c250b89462ebc3c5c47
brunodecruz <brunodecruz.05@gmail.com>
2023-08-11 16:25:18 -0300

    divide in modules

---
+++ TP1/City.hs
@@ -0,0 +1,9 @@
+-- City.hs
+module City ( City, newC, nameC, distanceC ) where
+import Point
+
+data City = Cit String Point deriving (Eq, Show)
+
+newC :: String -> Point -> City
+nameC :: City -> String
+distanceC :: City -> City -> Float
---
+++ TP1/main.hs
@@ -0,0 +1,6 @@
+import  City
+import  Link
+import  Tunel
+import  Region
+import  Point
+import Quality
