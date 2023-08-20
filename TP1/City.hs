46d60c1583910df60d52a2ed519e4b39b6c120da
brunodecruz <brunodecruz.05@gmail.com>
2023-08-20 12:19:09 -0300

    vivar peron

--- TP1/City.hs
+++ TP1/City.hs
@@ -2,7 +2,7 @@
 module City ( City, newC, nameC, distanceC ) where
 import Point
 
-data City = Cit String Point deriving (Eq, Show)
+data City = Cit String Point deriving (Eq, Show, Ord)
 
 newC :: String -> Point -> City
 newC name point = Cit name point
