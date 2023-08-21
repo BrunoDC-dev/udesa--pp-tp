116d54830c2c30fabd5e30b6abd9a78766d77d87
joacocade <joacocadeiras@gmail.com>
2023-08-21 02:13:43 -0300

    mas tests, version minimum capacity mas clara

--- TP1/Tunel.hs
+++ TP1/Tunel.hs
@@ -34,6 +34,5 @@ connectsT city1 city2 (Tun links) = checkCitiesInNonRepeated links city1 city2
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
 usesT link (Tun links) = link `elem` links  
 
-
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
 delayT (Tun links) = sum (map delayL links) -- sumo todas las demoras de los links que componen el tunel
--- TP1/main.hs
+++ TP1/main.hs
@@ -1,7 +1,6 @@
 import  City
 import  Link
 import  Tunel
---import  Region
 import  Point
 import Quality
 import  Region
@@ -57,17 +56,33 @@ tests = [newP 2 2 == Poi 2 2,
 
          newL testCity2 testCity testQuality1 == Lin testCity2 testCity testQuality1, 
          connectsL testCity (newL testCity2 testCity testQuality1) == True,
+         connectsL testCity (newL testCity2 testCity3 testQuality1) == False,
          linksL testCity testCity2 (newL testCity2 testCity testQuality1) == True,
+         linksL testCity testCity2 (newL testCity2 testCity3 testQuality1) == False,
          capacityL (newL testCity2 testCity testQuality1) == 1,
          delayL (newL testCity2 testCity testQuality1) == 0.1,
 
          newT [testlink, testlink2] == Tun [testlink, testlink2], 
-         connectsT testCity testCity2 (newT [testlink, testlink2]) == True,
-         
+         connectsT testCity testCity2 (newT [testlink]) == True,
+         connectsT testCity4 testCity2 (newT [testlink]) == False,
+         connectsT testCity4 testCity2 (newT [testlink, testlink2]) == True,
+         connectsT testCity5 testCity (newT [testlink, testlink3, testlink4]) == True,
+         connectsT testCity5 testCity (newT [testlink, testlink2, testlink3]) == False,
+         usesT testlink (newT [testlink, testlink2]) == True,
+         delayT (newT [testlink, testlink2]) == 0.3,
 
          newR == Reg [] [] [],
          foundR emptyTestRegion testCity == Reg [testCity] [] [],
-         testLinksRegion == Reg [] [testlink2,testlink4,testlink,testlink3] [],
-         testCitiesRegion == Reg [testCity4,testCity2,testCity] [] [],
-         testFullRegion == Reg [testCity4,testCity2,testCity] [testlink2,testlink4,testlink,testlink3] [testTunel2,testTunel], 
+         foundT emptyTestRegion testTunel == Reg [] [] [testTunel],
+         foundL emptyTestRegion testlink == Reg [] [testlink] [],
+         sameRegion (Reg [testCity, testCity2, testCity3] [] []) testCity testCity2 == True,
+         sameRegion (Reg [testCity, testCity3] [] []) testCity testCity2 == False,
+         minimumCapacity [testlink, testlink2, testlink3, testlink4] == 1,
+         linkExists testCity testCity2 [testlink2, testlink3, testlink, testlink4] == True,
+
          True]
+
+state tests | and tests = "Correcto!"
+            | otherwise = "Hubo errores :("
+
+main = print tests
