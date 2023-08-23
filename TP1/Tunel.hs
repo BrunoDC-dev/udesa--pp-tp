b33a31e26096328f7ff7b4081193fc7d8fbb8e9c
brunodecruz <brunodecruz.05@gmail.com>
2023-08-23 12:07:26 -0300

    julieta

--- TP1/Tunel.hs
+++ TP1/Tunel.hs
@@ -15,11 +15,21 @@ newT = Tun
 
 connectsT :: City -> City -> Tunel -> Bool
 connectsT city1 city2 (Tun links) 
-  |distanceC city1 city2 == 0 = error "no es posible crear un tunel con la misma ciudad"
-  |length links < 1 = error "`no es posible crear un tunel con menos de un link"
-  |connectsL city1 (head links) && connectsL city2 (last links) = True
-  |connectsL city2  (head links) && connectsL city1 (last links) =True
-  |otherwise = error "Las ciudades no son los extremos del tunel"
+  | distanceC city1 city2 == 0 = error "No es posible crear un túnel con la misma ciudad."
+  | length links < 1 = error "No es posible crear un túnel con menos de un link."
+  | connectsL city1 (head links) && connectsL city2 (last links) && notInSecondLink && notInSecondToLastLink = True
+  | connectsL city2 (head links) && connectsL city1 (last links) && notInSecondLink && notInSecondToLastLink = True
+  | otherwise = False
+  where
+    notInSecondLink = not (city2 `connectedWith` secondLink)
+    notInSecondToLastLink = not (city1 `connectedWith` secondToLastLink)
+
+    secondLink = if length links >= 2 then links !! 1 else error "No hay suficientes links en el túnel."
+    secondToLastLink = if length links >= 2 then last (init links) else error "No hay suficientes links en el túnel."
+    
+    connectedWith :: City -> Link -> Bool
+    connectedWith city link = city `connectsL` link
+
 
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
 usesT link (Tun links) = link `elem` links 
