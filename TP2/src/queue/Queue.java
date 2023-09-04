02e1c899e6fe95c8badd69b5cab82e7a01295e1c
brunodecruz <brunodecruz.05@gmail.com>
2023-09-04 12:56:20 -0300

    v1.0.0

---
+++ TP2/src/queue/Queue.java
@@ -0,0 +1,38 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+public class Queue {
+
+    private LinkedList<Object> cargoList = new LinkedList<>();
+
+    public boolean isEmpty() {
+        return cargoList.isEmpty();
+    }
+
+    public Queue add(Object cargo) {
+        cargoList.addLast(cargo);
+        return this;
+    }
+
+    public Object take() {
+        try {
+            return cargoList.removeFirst();
+        } catch (NoSuchElementException e) {
+            throw new Error("Queue is empty");
+        }
+    }
+
+    public Object head() {
+        try {
+            return cargoList.getFirst();
+        } catch (NoSuchElementException e) {
+            throw new Error("Queue is empty");
+        }
+    }
+
+    public int size() {
+        return cargoList.size();
+    }
+}
