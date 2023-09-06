7c88342e9d5536db9abdf49993f608d31d2d45c3
brunodecruz <brunodecruz.05@gmail.com>
2023-09-06 12:00:01 -0300

    djbfhdb

--- TP2/src/queue/Queue.java
+++ TP2/src/queue/Queue.java
@@ -1,39 +1,35 @@
 
     package queue;
 
-    import java.util.LinkedList;
-    import java.util.NoSuchElementException;
-    
-    public class Queue {
-    
-        private LinkedList<Object> cargoList = new LinkedList<>();
-    
-        public boolean isEmpty() {
-            return cargoList.isEmpty();
-        }
-    
-        public Queue add(Object cargo) {
-            cargoList.addLast(cargo);
-            return this;
-        }
-    
-        public Object take() {
-            try {
-                return cargoList.removeFirst();
-            } catch (NoSuchElementException e) {
-                throw new Error("Queue is empty");
-            }
-        }
-    
-        public Object head() {
-            try {
-                return cargoList.getFirst();
-            } catch (NoSuchElementException e) {
-                throw new Error("Queue is empty");
-            }
-        }
-    
-        public int size() {
-            return cargoList.size();
-        }
+
+public class Queue {
+    private QueueState state;
+
+    public Queue() {
+        this.state = new EmptyQueue();
     }
+
+    public Queue add(String element) {
+        return new Queue(new NonEmptyQueue(element, state));
+    }
+
+    private Queue(QueueState state) {
+        this.state = state;
+    }
+
+    public boolean isEmpty() {
+        return state.isEmpty();
+    }
+
+    public String head() {
+        return state.head();
+    }
+
+    public Queue take() {
+        return new Queue(state.take());
+    }
+
+    public int size() {
+        return state.size();
+    }
+}
