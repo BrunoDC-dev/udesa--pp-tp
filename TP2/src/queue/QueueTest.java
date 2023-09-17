9d450e7764cfd7c6b74574985d40a47eabd3c467
Joaquín Cadeiras <joacocadeiras@gmail.com>
2023-09-17 01:18:24 -0300

    refactors, eliminado codigo repetido
    newQueueWith(element) reemplaza newQueue().add(element), y newQueue() renombrada a newEmptyQueue()

--- TP2/src/queue/QueueTest.java
+++ TP2/src/queue/QueueTest.java
@@ -46,49 +46,41 @@ public class QueueTest {
   }
 
   @Test public void test07HeadReturnsFirstAddedObject() {
-    Queue queue = newQueue();
-
-    queue.add( firstAddedObject );
+    Queue queue = newQueueWith( firstAddedObject );
     queue.add( secondAddedObject );
 
     assertEquals( queue.head(), firstAddedObject );
   }
 
   @Test public void test08HeadDoesNotRemoveObjectFromQueue() {
-    Queue queue = newQueue(); //
-    queue.add( addedObject ); //
-    // Queue queue = newQueue().add( addedObject );
+    Queue queue = newQueueWith( addedObject );
     assertEquals( 1, queue.size() );
     queue.head();
     assertEquals( 1, queue.size() );
   }
 
   @Test public void test09SizeRepresentsObjectInTheQueue() {
-    assertEquals( 2, newQueue().add(firstAddedObject ).add( secondAddedObject).size() );
+    assertEquals( 2, newQueueWith( firstAddedObject ).add( secondAddedObject ).size() );
   }
 
   @Test public void test10CanNotTakeWhenThereAreNoObjectsInTheQueue() {
-    Queue queue = newQueue();
+    Queue queue = newEmptyQueue();
     assertThrows(Error.class, () -> queue.take(), "Queue is empty");
   }
 
   @Test public void test11CanNotTakeWhenThereAreNoObjectsInTheQueueAndTheQueueHadObjects() {
-    Queue queue = newQueue(); //
-    queue.add( addedObject ); //
-    // Queue queue = newQueue().add( addedObject );
+    Queue queue = newQueueWith( addedObject );
     queue.take();
     assertThrows(Error.class, () -> queue.take(), "Queue is empty");
   }
 
   @Test public void test12CanNotHeadWhenThereAreNoObjectsInTheQueue() {
-    Queue queue = newQueue();
+    Queue queue = newEmptyQueue();
     assertThrows(Error.class, () -> queue.head(), "Queue is empty");
   }
 
   @Test public void test13CanNotHeadWhenThereAreNoObjectsInTheQueueAndTheQueueHadObjects() {
-    Queue queue = newQueue(); //
-    queue.add( addedObject ); //
-    // Queue queue = newQueue().add( addedObject );
+    Queue queue = newQueueWith( addedObject );
     queue.take();
     assertThrows(Error.class, () -> queue.head(), "Queue is empty");
   }
@@ -97,8 +89,12 @@ public class QueueTest {
   private String secondAddedObject = "second";
   private String addedObject = "something";
   
-  private Queue newQueue() {
-        return new Queue();
+  private Queue newEmptyQueue() {
+    return new Queue();
+  }
+
+  private Queue newQueueWith(String object) {
+    return new Queue().add( object );
   }
 
 }
