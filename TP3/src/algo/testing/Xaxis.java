cf82d612009cee9e9988b50c119192f4158931bf
Joaquín Cadeiras <joacocadeiras@gmail.com>
2023-10-09 12:58:49 -0300

    tests

---
+++ TP3/src/algo/testing/Xaxis.java
@@ -0,0 +1,14 @@
+package algo.testing;
+
+public class Xaxis extends Nemo {
+    private String direction = "positive";
+    private int value = 0;
+
+    public void moveForward() {
+        if (direction == "positive") {
+            value++;
+        } else {
+            value--;
+        }
+    }
+}
